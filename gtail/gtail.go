0eccbbaacb24b85e33b003c020ee21fa70e2152c
urgen sherpa <sherpaurgen@users.noreply.github.com>
2023-05-15 22:53:38 +0545

    go cmdline tool similar to 'tail -f' in linux

---
+++ gtail/gtail.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
+
+func gtail(filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	//https://cs.opensource.google/go/go/+/master:src/io/fs/fs.go;l=151?q=FileInfo&sq=&ss=go
+	//
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return err
+	}
+	//	Size() int64        // length in bytes for regular files; system-dependent for others
+	fileSize := fileInfo.Size()
+	//here filezise is offset which is the number of bytes to move the file pointer from its current position. For example, if you call file.Seek(10, 0), the file pointer will be moved 10 bytes from the beginning of the file.
+	// here whence = 2
+	//0: The file offset is measured from the beginning of the file.
+	//1: The file offset is measured from the current position of the file pointer.
+	//2: The file offset is measured from the end of the file.
+	_, err = file.Seek(fileSize, 2)
+	if err != nil {
+		return err
+	}
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err.Error() != "EOF" {
+				return err
+			}
+		}
+
+		if line != "" {
+			fmt.Print(line) //print line if it is not empty
+		}
+
+		// Sleep for small interval 50ms before reading again
+		time.Sleep(50 * time.Millisecond)
+	}
+}
