ac2155e9d9b8adfaaeb46b32b151910f668417eb
urgen sherpa <sherpaurgen@users.noreply.github.com>
2023-05-17 22:25:52 +0545

    revised

--- gtail/gtail.go
+++ gtail/gtail.go
@@ -5,45 +5,59 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"time"
 )
 
 func gtail(filename string) error {
+	const bufferSize = 70 //byte
 	file, err := os.Open(filename)
 	if err != nil {
 		return err
 	}
 	//https://cs.opensource.google/go/go/+/master:src/io/fs/fs.go;l=151?q=FileInfo&sq=&ss=go
-	//
+
 	fileInfo, err := file.Stat()
 	if err != nil {
 		return err
 	}
-	//	Size() int64        // length in bytes for regular files; system-dependent for others
+	/*
+		Size() int64        // length in bytes for regular files; system-dependent for others
+		here filesize is offset which is the number of bytes to move the file pointer from its current position. For example, if you call file.Seek(10, 0), the file pointer will be moved 10 bytes from the beginning of the file.
+		here whence = 2
+		0: The file offset is measured from the beginning of the file.
+		1: The file offset is measured from the current position of the file pointer.
+		2: The file offset is measured from the end of the file.
+	*/
 	fileSize := fileInfo.Size()
-	//here filezise is offset which is the number of bytes to move the file pointer from its current position. For example, if you call file.Seek(10, 0), the file pointer will be moved 10 bytes from the beginning of the file.
-	// here whence = 2
-	//0: The file offset is measured from the beginning of the file.
-	//1: The file offset is measured from the current position of the file pointer.
-	//2: The file offset is measured from the end of the file.
-	_, err = file.Seek(fileSize, 2)
+	offsetpos, err := file.Seek(fileSize, 2)
+	fmt.Println("first offset position:")
+	fmt.Println(offsetpos)
 	if err != nil {
 		return err
 	}
-	reader := bufio.NewReader(file)
+	reader := bufio.NewReaderSize(file, bufferSize)
 	//	defaultBufSize = 4096 byte
 	for {
+		_, err = file.Seek(fileSize, 2)
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			if err.Error() != "EOF" {
 				return err
 			}
 		}
-
-		if line != "" {
-			fmt.Print(line) //print line if it is not empty
+		fmt.Println(offsetpos)
+		fmt.Print(line) //print line if it is not empty
+		newFileSize, err := file.Stat()
+		fmt.Println("newfilesize: " + strconv.FormatInt(newFileSize.Size(), 10))
+		if err != nil {
+			return err
 		}
 
+		if newFileSize.Size() != fileSize {
+			fileSize = newFileSize.Size()
+			continue // Continue to the next iteration if new lines are appended
+		}
 		// Sleep for small interval 50ms before reading again
 		time.Sleep(900 * time.Millisecond)
 	}
