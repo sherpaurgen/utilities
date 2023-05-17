1efe45deb03b70adb7362d9d6bd7e5660f105ada
urgen sherpa <sherpaurgen@users.noreply.github.com>
2023-05-18 00:21:07 +0545

    Added docs link , need to handle file that is truncated or moved

--- gtail/gtail.go
+++ gtail/gtail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -27,10 +28,14 @@ func gtail(filename string) {
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		newFileSize := newFileInfo.Size()
 
 		if newFileSize < fileSize {
-			// File truncated, seek to the beginning
+			/* File truncated, seek to the beginning
+			 whence: 0 means relative to the origin of the file, 1 means relative to the current offset,
+			and 2 means relative to the end
+			*/
 			_, err = file.Seek(0, 0)
 			if err != nil {
 				log.Fatal(err)
@@ -44,9 +49,11 @@ func gtail(filename string) {
 
 			scanner := bufio.NewScanner(file)
 			for scanner.Scan() {
-				log.Println(scanner.Text())
+				fmt.Println(scanner.Text())
 			}
-
+			/* https://cs.opensource.google/go/go/+/refs/tags/go1.20.4:src/bufio/scan.go;l=29
+			this shows that if scanner gets EOF a <nil> is set to scanner.Err()
+			*/
 			if scanner.Err() != nil {
 				log.Fatal(scanner.Err())
 			}
@@ -55,7 +62,7 @@ func gtail(filename string) {
 			fileSize = newFileSize
 		}
 
-		time.Sleep(1 * time.Second)
+		time.Sleep(900 * time.Millisecond)
 	}
 }
 
